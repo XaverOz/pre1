fb06a4c06ac0dbf1e30f2c4976f42036f579b0c3
Anatoly Zaharov <xaverozz@gmail.com>
2019-12-17 14:06:53 +0500

    конфиругация сервлетов аннотациями (вместо xml)

--- src/main/java/EditServlet.java
+++ src/main/java/EditServlet.java
@@ -1,10 +1,12 @@
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
+@WebServlet(value = "/edit")
 public class EditServlet extends HttpServlet {
     UserDBService userDBService = new UserDBService();
 
--- src/main/java/UsersServlet.java
+++ src/main/java/UsersServlet.java
@@ -1,10 +1,12 @@
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
+@WebServlet(value = "/users")
 public class UsersServlet extends HttpServlet {
     UserDBService userDBService = new UserDBService();
 
