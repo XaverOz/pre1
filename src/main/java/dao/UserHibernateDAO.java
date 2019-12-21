dc5bf1a744c30c496f6d37365139727a5880b0ad
Anatoly Zaharov <xaverozz@gmail.com>
2019-12-21 09:48:21 +0500

    удалил ненужные commitы

--- src/main/java/dao/UserHibernateDAO.java
+++ src/main/java/dao/UserHibernateDAO.java
@@ -49,41 +49,33 @@ public class UserHibernateDAO implements UserDAO {
     }
     public boolean deleteUser(long id) {
         Session session = sessionFactory.openSession();
-        session.beginTransaction();
         Query query = session.createQuery("Delete FROM User where id =:id");
         query.setParameter("id", id);
         query.executeUpdate();
-        session.getTransaction().commit();
         session.close();
         return true;
     }
     public void updateUser(User user) {
         Session session = sessionFactory.openSession();
-        session.beginTransaction();
         Query query = session.createQuery("UPDATE User SET age =:age, name =:name  WHERE id =:id");
         query.setParameter("age", user.getAge());
         query.setParameter("name", user.getName());
         query.setParameter("id", user.getId());
         query.executeUpdate();
-        session.getTransaction().commit();
         session.close();
     }
     public User getUserById(long id) {
         Session session = sessionFactory.openSession();
-        session.beginTransaction();
         Query query = session.createQuery("FROM User where id =:id");
         query.setParameter("id", id);
         User user = (User) query.uniqueResult();
-        session.getTransaction().commit();
         session.close();
         return user;
     }
     public List<User> getAllUser() {
         Session session = sessionFactory.openSession();
-        session.beginTransaction();
         Query query = session.createQuery("FROM User");
         List<User> users = query.list();
-        session.getTransaction().commit();
         session.close();
         return users;
     }
